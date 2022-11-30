464f924c8a61e982091a5a94929fc76f872efecc
Daniella Mesquita <mesquitadaniella@proton.me>
2022-11-30 03:02:28 -0300

    Move function scripts to js/ folder

--- main.js
+++ main.js
@@ -4,11 +4,11 @@ const myArgs = process.argv.slice(2);
 const { exec } = require("child_process");
 switch (myArgs[0]) {
   case 'encrypt':
-    execontent = 'node encrypt.js' + " '" + myArgs[1] + "' '" + myArgs[2] + "'"
+    execontent = 'node js/encrypt.js' + " '" + myArgs[1] + "' '" + myArgs[2] + "'"
     exec(execontent, (error, stdout, stderr) => {if (error){console.log(`error: ${error.message}`);return;}if (stderr){console.log(`stderr: ${stderr}`);return;}console.log(stdout);});
     break;
   case 'decrypt':
-    execontent = 'node decrypt' + " '" + myArgs[1] + "' '" + myArgs[2] + "'"
+    execontent = 'node js/decrypt.js' + " '" + myArgs[1] + "' '" + myArgs[2] + "'"
     exec(execontent, (error, stdout, stderr) => {if (error){console.log(`error: ${error.message}`);return;}if (stderr){console.log(`stderr: ${stderr}`);return;}console.log(stdout);});
     break;
   default:
