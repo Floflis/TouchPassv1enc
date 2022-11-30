f77fd818c6793dac0770d43a40c6704322cc9aec
Daniella Mesquita <mesquitadaniella@proton.me>
2022-11-30 01:29:28 -0300

    Updates

--- decrypt.js
+++ decrypt.js
@@ -2,8 +2,6 @@
 const CryptoJS = require('crypto-js');
 
 let secretKey = 'my secretkey 123'
-//let enc = 'HcMV322sPPrFonlvXZ5pvFpXABVrxYVAXrEr9vcu7TIFOPNQVGq5svEv3959sRDxkDHB/nav80iGMPBiz8bDeA=='
-
 let keyUtf8 = CryptoJS.enc.Utf8.parse(secretKey)
 
 ENC = CryptoJS.AES.decrypt('HcMV322sPPrFonlvXZ5pvFpXABVrxYVAXrEr9vcu7TIFOPNQVGq5svEv3959sRDxkDHB/nav80iGMPBiz8bDeA==', keyUtf8, { mode: CryptoJS.mode.ECB,  keySize: 128 });
@@ -11,18 +9,4 @@ ENC = CryptoJS.AES.decrypt('HcMV322sPPrFonlvXZ5pvFpXABVrxYVAXrEr9vcu7TIFOPNQVGq5
 ENC = CryptoJS.enc.Utf8.stringify(ENC).toString();
 // credits for above: https://stackoverflow.com/a/60295551/5623661
 
-//console.log(ENC, '--->');
 console.log(ENC);
-
-
-
-
-
-//const CryptoJS = require('crypto-js');
-
-//var bytes  = CryptoJS.AES.decrypt('HcMV322sPPrFonlvXZ5pvFpXABVrxYVAXrEr9vcu7TIFOPNQVGq5svEv3959sRDxkDHB/nav80iGMPBiz8bDeA==', 'my secretkey 123');
-
-//var originalText = bytes.toString(CryptoJS.enc.Utf8);
-
-//console.log(originalText, '--->');
-// credits for above: https://stackoverflow.com/questions/60295154/aes-128-ecb-decryption-nodejs
--- encrypt.js
+++ encrypt.js
@@ -1,29 +1,9 @@
 // Require the core node modules.
 const CryptoJS = require('crypto-js');
 
-//let secretKey = 'my secretkey 123'
+let secretKey = 'my secretkey 123'
+let keyUtf8 = CryptoJS.enc.Utf8.parse(secretKey)
 
-//let keyUtf8 = CryptoJS.enc.Utf8.parse(secretKey)
+let ciphertext = CryptoJS.AES.encrypt('MODE=UL&TYPE=V&LID=abcdef&LPW=password&AN=20022002', keyUtf8, { mode: CryptoJS.mode.ECB,  keySize: 128 }).toString();
 
-//ENC = CryptoJS.AES.encrypt('MODE=UL&TYPE=V&LID=abcdef&LPW=password&AN=20022002', keyUtf8, { mode: CryptoJS.mode.ECB,  keySize: 128 });
-
-//ENC = CryptoJS.enc.Utf8.stringify(ENC).toString();
-
-//console.log(ENC);
-
-
-
-// Encrypt
-var secretKey = 'my secretkey 123'
-var keyUtf8 = CryptoJS.enc.Utf8.parse(secretKey)
-
-var ciphertext = CryptoJS.AES.encrypt('MODE=UL&TYPE=V&LID=abcdef&LPW=password&AN=20022002', keyUtf8, { mode: CryptoJS.mode.ECB,  keySize: 128 }).toString();
-
-// Decrypt
-//var bytes  = CryptoJS.AES.decrypt(ciphertext, 'my secretkey 123', { mode: CryptoJS.mode.ECB,  keySize: 128 });
-//var originalText = bytes.toString(CryptoJS.enc.Utf8);
-
-//console.log(bytes);
 console.log(ciphertext);
-//console.log(originalText); // 'my message'
-// credits for above: https://www.npmjs.com/package/crypto-js. it has been modified by daniella
